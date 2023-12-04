55f896466695799292e6e5f14ec11e260d6dddf1
Ruhan <ruhan.swanepoel17@gmail.com>
2023-12-04 09:10:23 +0200

    Used Math.random() for n

--- Stack/src/InspectionFactory.java
+++ Stack/src/InspectionFactory.java
@@ -1,17 +1,24 @@
 import entity.Inspection;
 import structure.LinkedStack;
 
+import java.util.Random;
+import java.util.Stack;
+
 public class InspectionFactory {
     public static void main(String[] args) {
         System.out.println("Inspection Factory is open! Initiating inspection...");
 
-        LinkedStack stack = new LinkedStack();
-        stack.push(2);
-        stack.push(1);
-        stack.push(0);
+        int n = (int) (Math.random() * 100);
 
-        System.out.println("Inspecting at #" + stack.pop());
-        System.out.println("Inspecting at #" + stack.pop());
-        System.out.println("Inspecting at #" + stack.pop());
+        LinkedStack stack = new LinkedStack();
+        for (int i = 0; i < n; i++) {
+            stack.push(n - i);
+        }
+
+        System.out.println("Inspection has " + n + " stations");
+
+        for (int i = 0; i < n; i++) {
+            System.out.println("Inspecting at station #" + stack.pop());
+        }
     }
 }
